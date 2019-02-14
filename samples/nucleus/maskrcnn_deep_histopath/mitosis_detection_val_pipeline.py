d3feca715895f238f821d770890e3a5c85254e14
Fei Hu <hufei68@gmail.com>
2019-02-13 16:17:41 -0800

    Change augmentation_number param to be 1

--- samples/nucleus/maskrcnn_deep_histopath/mitosis_detection_val_pipeline.py
+++ samples/nucleus/maskrcnn_deep_histopath/mitosis_detection_val_pipeline.py
@@ -62,7 +62,7 @@ class ValConfig(object):
     # the mitosis patch should be bigger than mitosis tile as the patch will be
     # augmented (e.g. rotation) and crop into the mitosis tile.
     mitosis_patch_size = 72
-    augmentation_number = 64
+    augmentation_number = 1
     mitosis_tile_size = 64
     mitosis_classification_prefetch = 512  # parameter for tf.dataset.prefetch
     mitosis_classification_num_parallel_calls = 8  # parameter for tf.dataset.map
